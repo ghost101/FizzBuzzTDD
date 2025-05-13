7d72df391ab1a170b369067e832361f61694404e
ghost1963 <abdul.hamid@entityltd.com>
2025-05-13 11:05:54 +0100

    Add project files.

---
+++ FizzBuzzTDD.Tests/FizzBuzzTests.cs
@@ -0,0 +1,62 @@
+﻿namespace FizzBuzzTDD.Tests
+{
+    public class FizzBuzzTests
+    {
+    //    [Fact]
+    //    public void ReturnNumAsString()
+    //    {
+    //        // Arrange
+    //        var fizzBuzz = new FizzBuzz();
+
+    //        // Act
+    //        string result = fizzBuzz.GetNum(1);
+
+    //        // Assert
+    //        Assert.Equal("1", result);
+
+    //    }
+
+    //    [Fact]
+    //    public void ReturnString_WhenDivisibleBy3()
+    //    {
+    //        var fizzBuzz = new FizzBuzz();
+    //        string result = fizzBuzz.GetNum(3);
+
+    //        Assert.Equal("Fizz", result);
+
+    //    }
+
+    //    [Fact]
+    //    public void ReturnString_WhenDivisibleBy5()
+    //    {
+
+    //        var fizzBuzz = new FizzBuzz();
+    //        string result = fizzBuzz.GetNum(5);
+
+    //        Assert.Equal("Buzz", result);
+    //    }
+
+    //    [Fact]
+    //    public void ReturnString_WhenDivisibleBy3and5()
+    //    {
+
+    //        var fizzBuzz = new FizzBuzz();
+    //        string result = fizzBuzz.GetNum(15);
+
+    //        Assert.Equal("FizzBuzz", result);
+    //    }
+
+        [Theory]
+        [InlineData(1, "1")]
+        [InlineData(3, "Fizz")]
+        [InlineData(5, "Buzz")]
+        [InlineData(45, "FizzBuzz")]
+        [InlineData(98, "98")]
+        public void GetNum_ReturnsCorrectFizzBuzzOutput(int input, string expected)
+        {
+            var fizzBuzz = new FizzBuzz();
+            string result = fizzBuzz.GetNum(input);
+            Assert.Equal(expected, result);
+        }
+    }
+}
---
+++ FizzBuzzTDD/Program.cs
@@ -0,0 +1,21 @@
+﻿using FizzBuzzTDD;
+
+var fizzBuzz = new FizzBuzz();
+
+for (int i = 1; i <= 100; i++)
+{
+    string output = fizzBuzz.GetNum(i);
+    CenterText(output);
+}
+Console.ReadLine();
+
+static void CenterText(string text)
+{
+    int textWidth = text.Length;
+    int leftPadding = (Console.WindowWidth - textWidth) / 2;
+
+    if (leftPadding > 0)
+    {
+        Console.WriteLine(new string(' ', leftPadding) + text);
+    }
+}
